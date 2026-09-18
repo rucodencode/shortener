04a48f7130ace3e1241a56df7184bc353607e139
vaceslavnesterenko <dev-vn@forsite.ru>
2026-09-18 08:58:43 +0300

    Инкремент 5: Добавление переменных окружения

--- internal/config/options.go
+++ internal/config/options.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type options struct {
@@ -14,6 +15,14 @@ func NewOptions() options {
 	flag.StringVar(&options.ServerAddress, "a", ":8080", "address and port to run server")
 	flag.StringVar(&options.BaseURL, "b", "http://localhost:8080", "base url")
 	flag.Parse()
+
+	if envServerAddress := os.Getenv("SERVER_ADDRESS"); envServerAddress != "" {
+		options.ServerAddress = envServerAddress
+	}
+
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		options.BaseURL = envBaseURL
+	}
 
 	return options
 }
